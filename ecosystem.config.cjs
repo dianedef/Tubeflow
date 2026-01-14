40a49781aed1aba959e4422d546471c08db6a7bd
Diane DEFORES <deforesd@gmail.com>
2026-01-14 22:56:43 +0000

    run

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,9 +3,9 @@ module.exports = {
     name: "tubeflow",
     cwd: "/root/tubeflow",
     script: "bash",
-    args: ["-c", "export PORT=3004 && flox activate -- yarn dev"],
+    args: ["-c", "export PORT=3002 && flox activate -- sh -c 'cd apps/web && yarn dev'"],
     env: {
-      PORT: 3004
+      PORT: 3002
     },
     autorestart: true,
     watch: false
